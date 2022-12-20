d4e4cf8dce8e6a28271fb93525991197293e63af
Antoine <antoine.blumenroeder@epita.fr>
2022-12-20 11:51:33 +0100

    github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import partytown from "@astrojs/partytown";
 import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [/* partytown(),  */svelte()]
+  integrations: [svelte()],
+  site: 'https://hermita-game.github.io'
 });
